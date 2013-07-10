349d528221b080455e3a48baa004e66e359dfa6c
Jonathan King <jonathanlking@me.com>
2013-07-10 17:06:41 +0100

    It should now work!

--- program.py
+++ program.py
@@ -2,35 +2,80 @@ import time
 import string
 import RPi.GPIO as GPIO
 
+delayTime = 0.1
+string = "Hello World"
+
 dot = [1]
 dash = [1,1,1]
 space = [0]
 
+
+# The length of a dot is one unit.
+# 2. A dash is three units.
+# 3. The space between parts of the same letter is one unit.
+# 4. The space between letters is three units.
+# 5. The space between words is seven units.
+
 a = dot + space + dash #Dot Dash
-b = []
-
-
-
-delayTime = 1
-
+b = dash + space + dot + space + dot + space + dot
+c = dash + space + dot + space + dash + space + dot
+d = dash + space + dot + space + dot
+e = dot
+f = dot + space + dot + space + dash + space + dot
+g = dash + space + dash + space + dot 
+h = dot + space + dot + space + dot + space + dot
+i = dot + space + dot
+j = dot + space + dash + space + dash + space + dash
+k = dash + space + dot + space + dash
+l = dot + space + dash + space + dot + space + dot 
+m = dash + space + dash 
+n = dash + space + dot 
+o = dash + space + dash + space + dash 
+p = dot + space + dash + space + dash + space + dot 
+q = dash + space + dash + space + dot + space + dash
+r = dot + space + dash + space + dot
+s = dot + space + dot + space + dot 
+t = dash
+u = dot + space + dot + space + dash
+v = dot + space + dot + space + dot + space + dash
+w = dot + space + dash + space + dash
+x = dash + space + dot + space + dot + space + dash
+y = dash + space + dot + space + dash + space + dash
+z = dash + space + dash + space + dot + space + dot
 
 
 def letterToMorseCodeBinary(letter):
-	print letter
+	if letter is 'a': return a
+	elif letter is 'b': return b
+	elif letter is 'c': return c
+	elif letter is 'd': return d
+	elif letter is 'e': return e
+	elif letter is 'f': return f
+	elif letter is 'g': return g
+	elif letter is 'h': return h
+	elif letter is 'i': return i
+	elif letter is 'j': return j
+	elif letter is 'k': return k
+	elif letter is 'l': return l
+	elif letter is 'm': return m
+	elif letter is 'n': return n
+	elif letter is 'o': return o
+	elif letter is 'p': return p
+	elif letter is 'q': return q
+	elif letter is 'r': return r
+	elif letter is 's': return s
+	elif letter is 't': return t
+	elif letter is 'u': return u
+	elif letter is 'v': return v
+	elif letter is 'w': return w
+	elif letter is 'x': return x
+	elif letter is 'y': return y
+	elif letter is 'z': return z
+
+
 	
 def transmitMorseCodeFromList(list):
 
-# 	print list
-# 	
-
-# 	
-# 	GPIO.output(7, GPIO.HIGH)
-# 	time.sleep(delayTime)
-# 	GPIO.output(7, GPIO.LOW)
-# 	time.sleep(delayTime)
-# 	GPIO.output(7, GPIO.HIGH)
-# 	time.sleep(delayTime)
-	
 	for item in list:
 		if item is 0:
 			GPIO.cleanup()
@@ -49,20 +94,15 @@ def transmitMorseCodeFromList(list):
 					
 
 
-listOfCharacters = list("Hello World")
-lengthOfList = len(listOfCharacters)
+listOfCharacters = list(string)
 
-# print lengthOfList
-testCodeList = [1,0,0,1,1,1,0,0,1]
-transmitMorseCodeFromList(a)
+binaryArray = []
 
 for item in listOfCharacters:
-        letterToMorseCodeBinary(item)
+        binaryArray.extend(letterToMorseCodeBinary(item))
+        
+transmitMorseCodeFromList(binaryArray)
 
 
 
-# The length of a dot is one unit.
-# 2. A dash is three units.
-# 3. The space between parts of the same letter is one unit.
-# 4. The space between letters is three units.
-# 5. The space between words is seven units.
+
