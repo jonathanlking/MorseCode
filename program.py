d92420831e6b97aa08ca13ae5a1c52ef0c708121
Jonathan King <jonathanlking@me.com>
2013-07-10 15:18:46 +0100

    Initial Commit

---
+++ program.py
@@ -0,0 +1,14 @@
+dot = 1
+dash = [1,1,1]
+space = 0
+
+a = [dot, space, dash] #Dot Dash
+b = []
+
+
+
+# The length of a dot is one unit.
+# 2. A dash is three units.
+# 3. The space between parts of the same letter is one unit.
+# 4. The space between letters is three units.
+# 5. The space between words is seven units.
